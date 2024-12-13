f9ef45718b1bdb7bf95a96632ffbc6c3ad3d07b5
jenmwa <jenmwa@gmail.com>
2024-12-13 09:19:36 +0100

    add eslint and some rules to project

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser },
+  rules: {
+    'camelcase': 'error',
+    'no-var': 'error',
+    'no-unused-vars': 'warn',
+    'no-undef': 'error',
+    // 'no-console': 'warn',
+    'eqeqeq': ['error', 'always'],
+    'curly': 'error',
+  },},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
